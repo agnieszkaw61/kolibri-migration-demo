5e1da078a4fe5e5313a30c036c2f2f42aa204c09
Agnieszka Wrona <agnieszka.wrona@capgemini.com>
2024-04-12 13:23:43 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {defineCustomElements} from '@public-ui/components/dist/loader';
+import {BMF} from '@public-ui/themes';
+import {KoliBriDevHelper, register} from '@public-ui/components';
+import {customAlert} from './customAlert';
 
 @NgModule({
   declarations: [
@@ -13,4 +16,12 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    register(BMF, defineCustomElements)
+      .then(() => {
+        KoliBriDevHelper.patchTheme('bmf', customAlert);
+      })
+      .catch(console.warn);
+  }
+}
